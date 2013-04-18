a42bf7e7486093cde92a1a0051c0d1307d7a17a8
jaideep <jaideepk@thoughtworks.com>
2013-04-18 11:57:24 +0530

    jaideep|started with connecting to server

--- src/spike/Unicorn.java
+++ src/spike/Unicorn.java
@@ -1,11 +1,18 @@
 package spike;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jaideep
- * Date: 4/18/13
- * Time: 11:07 AM
- * To change this template use File | Settings | File Templates.
- */
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Unicorn {
+    public static void main(String[] args) {
+        try{
+            ServerSocket serverSocket = new ServerSocket(8080);
+        }
+        catch(IOException e) {
+            System.out.println("Could not listen on port: 8080");
+            e.printStackTrace();
+        }
+    }
+
+
 }
