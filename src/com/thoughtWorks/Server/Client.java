d38dde44c72d1e14f8e1a984f31bdf8172537ad8
sandeepsinha <sinha.sandeep01@gmail.com>
2013-04-24 00:58:26 +0530

    Sandeep | Created a client class to store clint information.

---
+++ src/com/thoughtWorks/Server/Client.java
@@ -0,0 +1,11 @@
+package com.thoughtWorks.Server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sandeepk
+ * Date: 4/23/13
+ * Time: 9:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Client {
+}
